269ef5e98085a520db3c170fc8b07de6dcb91489
bborkowsp <pawel.borkowski0704@gmail.com>
2023-11-09 13:28:41 +0100

    changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
        int result = calculator.addTwoNumber(1,2);
+       System.out.println(result);
     }
 }
